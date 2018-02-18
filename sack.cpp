4482585bc4fb887be90fe73ec7a6c522d2485dd6
Abdullah Al-Abd <abdaullah.el.abd@gmail.com>
2018-02-18 19:22:26 +0200

    Update sack.cpp

--- sack.cpp
+++ sack.cpp
@@ -6,8 +6,9 @@ vector<int> *vec[MAXN];
 vector<vector<int> >g(MAXN);
 int sz[MAXN];
 
-//Global Vars..
+//Global Vars:
 int k;
+int col[MAXN];
 
 //my sack
 ////////////////////////////
@@ -18,7 +19,7 @@ int subTreeSize(int v, int p){
     sz[v] = 1;
     for(auto u : g[v])
         if(u != p)
-            sz[v] += subTreeSize(u, v, lvl+1);
+            sz[v] += subTreeSize(u, v);
     return sz[v];
 }
 
@@ -28,16 +29,16 @@ void dfs(int v, int p, bool keep){
     for(auto u : g[v])
        if(u != p && sz[u] > mx)
            mx = sz[u], bigChild = u;
-           
+
     for(auto u : g[v])
        if(u != p && u != bigChild)
            dfs(u, v, 0);
-           
+
     if(bigChild != -1)
         dfs(bigChild, v, 1), vec[v] = vec[bigChild];
     else
         vec[v] = new vector<int> ();
-        
+
     cnt[ col[v] ]++;
     ////////////////////////////
     vec[v]->push_back(v);
@@ -50,14 +51,28 @@ void dfs(int v, int p, bool keep){
                 ////////////////////////////
                 vec[v] -> push_back(x);
             }
-    
+
     //now (*cnt[v])[c] is the number of vertices in subtree of vertex v that has color c. You can answer the queries easily.
     // note that in this step *vec[v] contains all of the subtree of vertex v.
-    
+
     if(keep == 0)
         for(auto u : *vec[v]){
             ////////////////////////////
             cnt[ col[u] ]--;
             ////////////////////////////
         }
+}
+
+int main(){
+    int n, u, v;
+    memset(sz, 0, sizeof sz);
+    scanf("%d%d", &n, &k);
+    for(int i = 1; i < n; i++){
+        scanf("%d%d", &u, &v);
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+    subTreeSize(1, -1);
+    dfs(1, -1, 1);
+
 }

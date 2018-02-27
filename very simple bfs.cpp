d8b683927f67dff6cfdae778ee28b71e80f54e77
Abdullah Al-Abd <abdaullah.el.abd@gmail.com>
2018-02-27 08:21:23 +0200

    Create very simple bfs.cpp

---
+++ very simple bfs.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool visited[MAXN];
+void bfs(int root){
+    memset(visited, 0, sizeof visited);
+    queue<int> q;
+    visited[root] = 1;
+    q.push(root);
+    while(!q.empty()){
+        //find adjacents
+            //if an adjacent not visted
+                //mark it as visited and push it to queue;
+    }
+}
+
+int main(){
+
+}

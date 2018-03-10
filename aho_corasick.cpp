7eb83c6a64b0242cd02467d7db5affbb8e3f8142
Abdullah Al-Abd <abdaullah.el.abd@gmail.com>
2018-03-10 13:01:51 +0200

    Update and rename aho.cpp to aho_corasick.cpp

--- aho_corasick.cpp
+++ aho_corasick.cpp
@@ -7,47 +7,51 @@ struct node {
     node *fail, *child[MAX_CHAR-MIN_CHAR+1];
     vector<int> patIdx;
     node() {
-        memset(child, 0, sizeof child);
+        memset(child, 0, sizeof child); // initialize to null
     }
 };
 
 struct aho_corasick {
 
-    int range, patCount;
-    node *root;
-    vector<int> ans;
-
+    int range, patCount; // the range betwean min and max, patCount is the number of inserted patterns.
+    node *root; // root of the trie
+    vector<int> ans; // for pattern number i how many times is appears in the main string.
+    
     aho_corasick() {
         patCount = 0;
         range = MAX_CHAR-MIN_CHAR+1;
         root = new node();
     }
 
-    void insert(char *str) {
+    void insert(char *str) { // insert a pattern in the trie
         node *cur = root;
         int ch;
         for(; *str; str++){
-            ch = *str-MIN_CHAR;
-            if(cur->child[ch] == 0)
-                cur->child[ch] = new node();
+        ch = *str-MIN_CHAR;
+        if(cur->child[ch] == 0)
+            cur->child[ch] = new node();
             cur = cur->child[ch];
         }
-        cur->patIdx.push_back(patCount++);
-        ans.push_back(0);
+        cur->patIdx.push_back(patCount++); // end node of a pattern
+        ans.push_back(0); // add another element ans to hold the answer of this pattern.
     }
 
-    void build() { //build failure on the trie
+    void build() { // build failure on the trie
         queue<node*> q;
+        // initalize failure of first level childs.
         for(int i = 0; i < range; i++)
             if(root->child[i])
                 q.push(root->child[i]), root->child[i]->fail = root;
-	    
+
         node *cur, *chld, *k;
         vector<int> *failList, *curList;
+        // bfs
         while(!q.empty()){
             cur = q.front();
             q.pop();
+
             for(int i = 0; i < range; i++){
+                // calculate failure of this child
                 chld = cur->child[i];
                 if(chld){
                     k = cur->fail;
@@ -59,17 +63,18 @@ struct aho_corasick {
                         chld->fail = k->child[i];
                     else
                         chld->fail = root;
+                    // insert failure list of patterns to cur list;
                     curList = &(chld->patIdx);
                     failList = &(chld->fail->patIdx);
                     curList->insert(curList->end(), failList->begin(), failList->end());
-
+                    // insert child to queue;
                     q.push(chld);
                 }
             }
         }
-    }
+	}
 
-    void answer(char *str) {
+	void answer(char *str) { // find inserted patterns in the main string str.
         node *cur = root, *k;
         int ch;
         for(; *str; str++){
@@ -79,14 +84,16 @@ struct aho_corasick {
                 if(k == root) break;
                 k = k->fail;
             }
+            
             if(k->child[ch])
                 cur = k->child[ch];
             else
                 cur = root;
-            for(int ptrn : cur->patIdx)
+            
+            for(int ptrn: cur->patIdx)
                 ans[ptrn]++;
         }
-    }
+	}
 
 };
 
@@ -97,20 +104,18 @@ int main(){
     cin >> k;
     while(k--){
         aho_corasick aho;
-
         cin >> S;
         cin >> q;
+        // get patterns
         for(int i = 0; i < q; i++){
             cin >> T;
             aho.insert(&T[0]);
         }
-
+        // build failure on trie
         aho.build();
-
+        //answer for this main string S
         aho.answer(&S[0]);
-
-        for(int counter : aho.ans){
+        for(int counter: aho.ans)
             puts(counter?"y":"n");
-        }
     }
 }

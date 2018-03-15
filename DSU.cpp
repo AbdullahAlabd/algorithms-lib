47a113395971931a1c6bf4568ff52353a059242d
Abdullah Al-Abd <abdaullah.el.abd@gmail.com>
2018-03-15 11:39:05 +0200

    Update DSU.cpp

--- DSU.cpp
+++ DSU.cpp
@@ -4,20 +4,20 @@ struct DSU{
  
     int id[MAXNODE], sz[MAXNODE];
  
-    DSU(){
-        for(int i =0;i<MAXNODE;i++)
-            id[i]=i,sz[i]=1;
+    DSU() {
+        for(int i = 0; i < MAXNODE; i++)
+            id[i] = i, sz[i] = 1;
     }
  
-    int root(int i){
+    int root(int i) {
         while(i != id[i])
             i = id[i] = i[id][id]; //equivalent to id[id[i]]
         return i;
     }
  
-    void unite(int a , int b){
-        a=root(a),b=root(b);
-        if(a==b) return;
+    void unite(int a , int b) {
+        a = root(a), b = root(b);
+        if(a == b) return;
         if (sz[a] < sz[b]) swap(a, b);
         if (sz[a] == sz[b]) ++sz[a];
         id[b] = a;

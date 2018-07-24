38828b12ce6637331b1ed97641b4417814d01e4d
Abdullah Al-Abd <abdaullah.el.abd@gmail.com>
2018-07-24 14:45:06 +0200

    Create linear_recurrence.cpp

---
+++ linear_recurrence.cpp
@@ -0,0 +1,9 @@
+// https://en.wikibooks.org/wiki/Linear_Algebra/Topic:_Linear_Recurrences
+//
+//    cn cn-1 ... cn-k         f(n)            f(n+1)
+//    1  0 ...... 0            f(n-1)          f(n)
+//    0  1 ...... 0      X     .         =     .
+//    .............            .               .
+//    .............            .               .
+//    0  0  0  1  0            f(n-k)          f(n-k+1)
+//

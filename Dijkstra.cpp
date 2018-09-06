ef3e10e43608466aa10821299aeed9768f0bf126
Abdullah Al-Abd <abdaullah.el.abd@gmail.com>
2018-09-06 16:46:07 +0200

    Update Dijkstra.cpp

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -1,51 +1,55 @@
-    const int MAXN = 100005;
-    const long long INF = 1e16; //greater than (max node wight) * number of nodes;
-    long long dist[MAXN];
-    int parent[MAXN];
-     
-    vector<pair<int,long long> > adj[MAXN];
-     
-    struct cmp{
-        bool operator () (pair<int,long long> a,pair<int,long long> b){
-            return (a.second>b.second);
-        }
-    };
-     
-    //find the shortest path from the source to every node reachable from it 
-    void dijkstra(int src){
-     
-        for(int i=0;i<MAXN;i++)
-            dist[i]=INF;
-     
-        priority_queue< pair<int, long long>, vector< pair<int, long long> >, cmp> q;
-        q.push({src,dist[src]=0});
-        parent[src]=-1;
-     
-        while(!q.empty()){
-            int node = q.top().first;
-            long long cost = q.top().second;
-            q.pop();
-     
-            if(dist[node]==cost){
-                for(auto it : adj[node]){
-                    int tnode = it.first;
-                    long long edgeCost = it.second;
-                    if(cost+edgeCost<dist[tnode]){
-                        parent[tnode] = node;
-                        q.push({tnode,dist[tnode]=cost+edgeCost});
-                    }
+typedef long long ll;
+
+const int MAXN = 1e5+5;
+const ll INF = 1e16+16; //greater than (max node wight) * number of nodes;
+
+ll dist[MAXN];
+int parent[MAXN];
+
+vector<pair<int,ll> > adj[MAXN];
+
+struct cmp {
+    bool operator () (pair<int, ll> a, pair<int, ll> b) {
+        return (a.second > b.second);
+    }
+};
+
+//find the shortest path from the source to every node reachable from it
+void dijkstra(int src){
+    for(int i = 0; i < MAXN; i++) {
+        dist[i] = INF;
+    }
+    priority_queue< pair<int, ll>, vector< pair<int, ll> >, cmp> q;
+    parent[src] = -1;
+    dist[src] = 0;
+    q.push({src, dist[src]});
+    pair<int, ll> p;
+    while(!q.empty()) {
+        p = q.top();
+        q.pop();
+        int node = p.first;
+        ll cost = p.second;
+        if(dist[node] == cost) {
+            for(auto it: adj[node]) {
+                int tnode = it.first;
+                ll edgeCost = it.second;
+                if(cost + edgeCost < dist[tnode]) {
+                    parent[tnode] = node;
+                    dist[tnode] = cost + edgeCost;
+                    q.push({tnode, dist[tnode]});
                 }
             }
         }
     }
-     
-    //get the path from node n to the source;
-    vector<int> getPath(int n){
-        vector<int> path;
-        while(n!=-1){
-            path.push_back(n);
-            n=parent[n];
-        }
-        reverse(path.begin(),path.end());
-        return path;
+}
+
+//get the path from node n to the source;
+vector<int> getPath(int v) {
+    vector<int> path;
+    while(v != -1) {
+        path.push_back(v);
+        v = parent[v];
     }
+    reverse(path.begin(), path.end());
+    return path;
+}

41c4eebd81ca026c4fc488fcddf96829947889c6
Abdullah Al-Abd <abdaullah.el.abd@gmail.com>
2018-09-09 23:21:43 +0200

    Update Mod_Inv.cpp

--- Mod_Inv.cpp
+++ Mod_Inv.cpp
@@ -1,20 +1,18 @@
 typedef long long ll;
-     
-int fast_power(ll x, ll n, int mod) { //where 1 < mod < max_int;
+
+int fastPower(ll x, ll n, int mod) { //where 1 < mod < max_int;
     ll ans = 1, base = x % mod;
-    while(n){
-        if(n&1){
+    while(n) {
+        if(n&1) {
             ans = (ans * base) % mod;
             n--;
-        }else{
-            base = (base * base) % mod;
-            n /= 2;
         }
+        base = (base * base) % mod;
+        n >>= 1;
     }
-    if (ans < 0) ans += mod;
-    return ans;
+    return (ans + mod) % mod;
 }
 
-int mod_inv(int a, int mod){
-    return fast_power(a, mod-2, mod); // value of (1/a)%mod;
+int modInv(int a, int mod) {
+    return fastPower(a, mod-2, mod); // value of (1/a)%mod;
 }

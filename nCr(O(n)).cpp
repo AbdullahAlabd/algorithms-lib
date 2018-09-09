b8dd4e7ef0eff958df6bb156160abf2c9f0298df
Abdullah Al-Abd <abdaullah.el.abd@gmail.com>
2018-09-09 23:48:56 +0200

    Update nCr(O(n)).cpp

--- nCr(O(n)).cpp
+++ nCr(O(n)).cpp
@@ -6,31 +6,28 @@ typedef long long ll;
 const int mod = 1e9+7, MAXN = 1000000;
 int fact[MAXN];
 
-int fast_power(ll x, ll n, int mod) {
+int fastPower(ll x, ll n, int mod) {
     ll ans = 1, base = x % mod;
-    while(n){
-        if(n&1){
+    while(n) {
+        if(n&1) {
             ans = (ans * base) % mod;
             n--;
-        }else{
-            base = (base * base) % mod;
-            n /= 2;
         }
+        base = (base * base) % mod;
+        n >>= 1;
     }
-    if (ans < 0) ans += mod;
-    return ans;
+    return (ans + mod) % mod;
 }
 
-int mod_inv(int a, int mod){
-    return fast_power(a, mod-2, mod);
+int modInv(int a, int mod) {
+    return fastPower(a, mod-2, mod);
 }
 
 int nCr(int n, int r) { // in O(1)
-    return (fact[n] * ((1ll * mod_inv(fact[n-r], mod) * mod_inv(fact[r], mod)) % mod)) % mod;
+    return (fact[n] * ((1ll * modInv(fact[n-r], mod) * modInv(fact[r], mod)) % mod)) % mod;
 }
 
-
-int main() { 
+int main() {
     // pre-computations in O(MAXN)
     fact[0] = 1;
     for(int i = 1; i < MAXN; i++) {

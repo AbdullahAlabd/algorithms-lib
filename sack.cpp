0df2d1aa7bbf9b058ff45c0adaecb56a2ac1763b
Abdullah Al-Abd <abdaullah.el.abd@gmail.com>
2018-10-02 00:58:55 +0200

    Update sack.cpp

--- sack.cpp
+++ sack.cpp
@@ -1,78 +1,80 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-const int MAXN = 50004;
+typedef long long ll;
+
+const int MAXN = 1e5+5;
+
+vector<int> g[MAXN]; //tree
 vector<int> *vec[MAXN];
-vector<vector<int> >g(MAXN);
-int sz[MAXN];
+int sz[MAXN]; // size of tree
 
-//Global Vars:
-int k;
-int col[MAXN];
+//////////////////////////
+// your sack data stucture.
+//////////////////////////
 
-//my sack
-////////////////////////////
-int cnt[MAXN];
-////////////////////////////
-
-int subTreeSize(int v, int p){
+int subTreeSize(int v, int p) {
     sz[v] = 1;
-    for(auto u : g[v])
-        if(u != p)
+    for(auto u: g[v])
+        if(u - p)
             sz[v] += subTreeSize(u, v);
     return sz[v];
 }
 
+void dfs(int v, int p, bool keep) {
 
-void dfs(int v, int p, bool keep){
-    int mx = -1, bigChild = -1;
-    for(auto u : g[v])
-       if(u != p && sz[u] > mx)
-           mx = sz[u], bigChild = u;
+    int mx = 0, big = -1;
 
-    for(auto u : g[v])
-       if(u != p && u != bigChild)
-           dfs(u, v, 0);
+    for(auto u: g[v])
+        if(u - p && sz[u] > mx)
+            big = u, mx = sz[u];
 
-    if(bigChild != -1)
-        dfs(bigChild, v, 1), vec[v] = vec[bigChild];
+    for(auto u: g[v])
+        if(u - p && u - big)
+            dfs(u, v, 0);
+
+    if(big+1)
+        dfs(big, v, 1), vec[v] = vec[big];
     else
         vec[v] = new vector<int> ();
 
-    cnt[ col[v] ]++;
-    ////////////////////////////
     vec[v]->push_back(v);
-    ////////////////////////////
-    for(auto u : g[v])
-        if(u != p && u != bigChild)
-            for(auto x : *vec[u]){
-                ////////////////////////////
-                cnt[ col[x] ]++;
-                ////////////////////////////
-                vec[v] -> push_back(x);
+
+    ////////////////
+    // add v to sack
+    ////////////////
+
+    for(auto u: g[v]) { // add light child's subtree.
+        if(u - p && u - big) {
+            for(auto x: *vec[u]) {
+                vec[v]->push_back(x);
+                ////////////////
+                // add x to sack
+                ///////////////
             }
-
-    //now (*cnt[v])[c] is the number of vertices in subtree of vertex v that has color c. You can answer the queries easily.
-    // note that in this step *vec[v] contains all of the subtree of vertex v.
-
-    if(keep == 0)
-        for(auto u : *vec[v]){
-            ////////////////////////////
-            cnt[ col[u] ]--;
-            ////////////////////////////
         }
+    }
+    ///////////////////////////////
+    // now we have answer of node v
+    ////////////////////////////////
+    if(!keep) {
+        // can reset sack vars here.
+        for(auto u: *vec[v]) {
+            ////////////////////
+            // remove u from sack
+            ////////////////////
+        }
+    }
 }
 
-int main(){
+int main() {
     int n, u, v;
-    memset(sz, 0, sizeof sz);
-    scanf("%d%d", &n, &k);
-    for(int i = 1; i < n; i++){
-        scanf("%d%d", &u, &v);
+    scanf("%d", &n);
+    for(int i = 0; i < n-1; i++) {
+        scanf("%d %d", &u, &v);
         g[u].push_back(v);
         g[v].push_back(u);
     }
     subTreeSize(1, -1);
     dfs(1, -1, 1);
-
 }

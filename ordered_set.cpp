1658fd64f6718b8a45b28bada1796260fd2c6877
Abdullah Al-Abd <abdaullah.el.abd@gmail.com>
2019-07-10 09:49:13 +0200

    Update ordered_set.cpp

--- ordered_set.cpp
+++ ordered_set.cpp
@@ -14,3 +14,14 @@ int main() {
 	cout << s.order_of_key(2) << endl; // the number of elements in the s less than 2
 	cout << *s.find_by_order(0) << endl; // print the 0-th smallest number in s(0-based)
 }
+
+
+/*
+Note on using less_equal as comparison function to use it as a multiset:
+
+_GLIBCXX_DEBUG must not be defined, otherwise some internal check will fail.
+find will always return end.
+lower_bound works like upper_bound in normal set (to return the first element > it)
+upper_bound works like lower_bound in normal set (to return the first element >= it)
+find_by_order and order_of_key works properly (unlike the 2 functions above).
+*/

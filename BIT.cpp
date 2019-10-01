1aafc4a2be7a69e181bd6f5c63346794faa37d43
Abdullah Al-Abd <abdaullah.el.abd@gmail.com>
2019-10-01 18:22:02 +0200

    Update BIT.cpp

--- BIT.cpp
+++ BIT.cpp
@@ -1,7 +1,7 @@
 const int MAXN = 1e5+5;
 int BIT[MAXN+1]; // max idx 1-based
 
-int get(int idx) {
+int get(int idx) { // get sum of element in range [1, idx].
     int sum = 0;
     while(idx) {
         sum += BIT[idx];
@@ -10,7 +10,7 @@ int get(int idx) {
     return sum;
 }
 
-void update(int idx, int val) {
+void update(int idx, int val) { // add val to element at idx
     while(idx <= MAXN) {
         BIT[idx] += val;
         idx += (idx & -idx);

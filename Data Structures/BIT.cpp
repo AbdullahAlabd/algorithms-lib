9cfafe81d688fa667cd812a107dc7ad44403eca0
Abdullah Alabd <abdaullah.el.abd@gmail.com>
2023-06-05 00:05:17 +0300

    Update BIT.cpp
    update: refactoring

--- Data Structures/BIT.cpp
+++ Data Structures/BIT.cpp
@@ -1,18 +1,20 @@
-const int MAXN = 1e5+5; // max possible idx + 2 at least 1-based
-int BIT[MAXN]; // 1-based
+struct BIT {
+    static const int MAXN = 1e5 + 5; // max possible idx + 2 at least 1-based
+    int data[MAXN] = {}; // 1-based
 
-int get(int idx) { // get sum of element in range [1, idx].
-    int sum = 0;
-    while(idx) {
-        sum += BIT[idx];
-        idx -= idx & -idx;
+    int get(int idx) { // get sum of element in range [1, idx].
+        int res = 0;
+        while(idx) {
+            res += data[idx];
+            idx -= idx & -idx;
+        }
+        return res;
     }
-    return sum;
-}
 
-void add(int idx, int val) { // add val to element at idx
-    while(idx < MAXN) {
-        BIT[idx] += val;
-        idx += idx & -idx;
+    void update(int idx, int val) { // add val to element at idx
+        while(idx < MAXN) {
+            data[idx] += val;
+            idx += idx & -idx;
+        }
     }
-}
+};

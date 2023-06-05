5da433be298c9777b74de25d7cc891073d9f276c
Abdullah Alabd <abdaullah.el.abd@gmail.com>
2023-06-05 18:04:18 +0300

    Update: refactored DSU.cpp

--- Data Structures/DSU.cpp
+++ Data Structures/DSU.cpp
@@ -1,27 +1,32 @@
-const int MAXN = 1e5+5;
-
 struct DSU {
-
-    int id[MAXN], sz[MAXN];
+    static const int MAXN = 1e5 + 5; 
+    int parent[MAXN], rank[MAXN];
 
     DSU() {
-        for(int i = 0; i < MAXN; i++) {
-            id[i] = i, sz[i] = 1;
+        for(int idx = 0; idx < MAXN; idx++) {
+            parent[idx] = idx, rank[idx] = 1;
         }
     }
 
-    int root(int i) {
-        while(i != id[i]) {
-            i = id[i] = id[id[i]]; // path compression.
+    int root(int idx) {
+        while(idx != parent[idx]) {
+            idx = parent[idx] = parent[parent[idx]];
         }
-        return i;
+        return idx;
     }
 
-    void unite(int a , int b) {
+    int unite(int a, int b) {
         a = root(a), b = root(b);
-        if(a == b) return;
-        if (sz[a] < sz[b]) swap(a, b); // union by rank.
-        if (sz[a] == sz[b]) ++sz[a];
-        id[b] = a;
+        if(a == b) {
+            return a;
+        }
+        if(rank[a] < rank[b]) {
+            swap(a, b);
+        }
+        if(rank[a] == rank[b]) {
+            rank[a]++;
+        }
+        parent[b] = a;
+        return a;
     }
 };
